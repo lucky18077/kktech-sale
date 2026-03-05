9b1e01454945e6fc5e9d3d2a1f389d760442a1e6
rohitclik0770 <rohit@clikzopinnovations.com>
2026-03-05 16:53:31 +0530

    Removed spatie package

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -41,11 +41,4 @@
       0 => 'Termwind\\Laravel\\TermwindServiceProvider',
     ),
   ),
-  'spatie/laravel-permission' => 
-  array (
-    'providers' => 
-    array (
-      0 => 'Spatie\\Permission\\PermissionServiceProvider',
-    ),
-  ),
 );
